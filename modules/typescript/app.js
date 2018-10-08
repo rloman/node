7fc863bbf9a081abd559bdbf74bc0954b331afb3
Raymond Loman <raymond@carpago.nl>
2018-10-08 13:36:57 +0200

    Remove the js files from repo

--- modules/typescript/app.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var tools_1 = require("./tools");
-var result = tools_1.foo();
-console.log(result);
--- modules/typescript/tools.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-exports.__esModule = true;
-function foo() {
-    console.log("This is foo speaking ... ");
-    return 42;
-}
-exports.foo = foo;
