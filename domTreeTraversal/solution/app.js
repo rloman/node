86fd892745f1f5f52508b6ea64182d11f9201478
Raymond Loman <raymond@carpago.nl>
2018-11-27 16:18:01 +0100

    Working and demoable version

--- domTreeTraversal/solution/app.js
+++ domTreeTraversal/solution/app.js
@@ -5,22 +5,14 @@ function perform() {
   // or better
 
   body = document.body;
-
-
-
-  for(let node of body.getElementsByTagName("*")) {
-  // for(let node of body.childNodes) {
-  // alert(node);
-  console.log(node.tagName);
-  if(node.tagName == "SPAN") {
-    console.log("Found span with content:"+node.innerHTML);
-  }
-  // alert(node.innerHTML);
-  if(node.tagName == "TD") {
-    alert(node.innerHTML);
-  }
-    if(node.nodeType == Node.TEXT_NODE) {
-      alert(node.tagName);
+  for(let node of body.getElementsByTagName("*")) { // deeper (recursive)
+  // for (let node of body.childNodes) {
+    if (node.tagName == "SPAN") {
+      console.log("Found span with content:" + node.innerHTML);
+    }
+    // alert(node.innerHTML);
+    if (node.tagName == "TD") {
+      console.log("Found TD node with content:" + node.innerHTML);
     }
   }
 }
