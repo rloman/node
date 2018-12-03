1cd931714248357bb380e7e5e4681828a7835972
Raymond Loman <raymond@carpago.nl>
2018-12-03 23:05:13 +0100

    Add ajax code of Felix

--- domTreeTraversal/app.js
+++ domTreeTraversal/app.js
@@ -1,6 +1,8 @@
 function perform() {
   let paragraphs = document.body.getElementsByTagName("p");
 
+  console.log(typeof(paragraphs));
+
   for(let paragraph of paragraphs) {
     console.log(paragraph);
     console.log(paragraph.innerHTML);
