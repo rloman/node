1e3d615f98ee84ec8d1b8ec2fbd72ffb45685bfd
Raymond Loman <raymond@carpago.nl>
2018-12-31 09:48:12 +0100

    Some p

--- jQueryDataTables/js/index.js
+++ jQueryDataTables/js/index.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
     $('#myTable').DataTable();
+
+    getData('http://jsonplaceholder.typicode.com/users');
 });
+
+function getData(api) {
+    //if(window.location.pathname == "/addBooking"){return;}
+    //var api = "http://localhost:8080/api/tables";
+    api = String(api);
+
+    console.log(api);
+ 
+    $.get(api, function(data){
+        console.log(data);
+        if (data){
+            
+             $("#dataTable").DataTable().clear();
+             $("#dataTable").DataTable().rows.add(data);
+             $("#dataTable").DataTable().columns.adjust().draw();
+             $("#dataTable").dataTable().api().ajax.reload();
+        }
+    });
+ }
