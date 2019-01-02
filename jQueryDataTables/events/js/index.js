ff8c59396b0991d32b73ff25fa548cb8ccce2ff1
Raymond Loman <raymond@carpago.nl>
2019-01-02 14:52:14 +0100

    Add events

---
+++ jQueryDataTables/events/js/index.js
@@ -0,0 +1,57 @@
+$(document).ready(function() {
+    initDataTable();
+    let api = 'http://jsonplaceholder.typicode.com/posts';
+
+    getData(api);
+    
+
+    $("#fetch").click(function() {
+        getData(api);
+    });
+
+    $("#clear").click(function() {
+        clear();
+    });
+});
+
+function initDataTable() {
+
+    columns = [
+        { "title": "ID",   "data": "id" }, 
+        { "title": "Title", "data": "title" },
+        { "title": "Body of post", "data": "body" },
+      
+        /*,
+        {  "render": function(data, type, row, meta){
+            return '<a title="Delete this table" <i class="fa fa-pencil-alt"></i> </a>';
+        } },*/
+    ];
+
+    let dataTable = $('#dataTable').DataTable( {
+        "order": [[ 0, "asc" ]],
+        "columns": columns
+    } );
+
+    $('#dataTable tbody').on('click', 'tr', function () {
+        var data = dataTable.row( this ).data();
+        alert( 'You clicked on '+data.id+'\'s row' );
+    } );
+}
+
+function clear(){
+    $("#dataTable").DataTable().clear();
+    $("#dataTable").DataTable().columns.adjust().draw();
+}
+
+function getData(api) {
+
+    // asynchronous REST GET
+    $.get(api, function(data){
+
+        if (data){
+             $("#dataTable").DataTable().clear();
+             $("#dataTable").DataTable().rows.add(data);
+             $("#dataTable").DataTable().columns.adjust().draw();
+        }
+    });
+ }
