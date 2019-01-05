433b7b3626b9040798129e7ec994ca27cfd015a2
Raymond Loman <raymond@carpago.nl>
2019-01-05 12:50:20 +0100

    Rock up to delete which rocks but fails if I use the parameter. see submitDelete function rloman tag

--- bootstrapModal/crud/js/index.js
+++ bootstrapModal/crud/js/index.js
@@ -1,66 +1,64 @@
 let api = 'http://jsonplaceholder.typicode.com/posts';
 
-$(document).ready(function() {
+$(document).ready(function () {
     initDataTable();
-   
+
 
     getData(api);
-    
 
-    $("#fetch").click(function() {
+
+    $("#fetch").click(function () {
         getData(api);
     });
 
-    $("#clear").click(function() {
+    $("#clear").click(function () {
         clear();
     });
-    $("#addBtn").on( 'click', function (){
-        document.getElementById("modal-title").innerHTML="Create a table";
+    $("#addBtn").on('click', function () {
+        document.getElementById("modal-title").innerHTML = "Create a table";
         document.getElementById("modalForm").reset();
-        $("#btnsubmit").attr('onclick', 'submitNew("' + api +'");');
+        $("#btnsubmit").attr('onclick', 'submitNew("' + api + '");');
         $('#modal').modal('toggle');
-        
-  });
+
+    });
 });
 
 function initDataTable() {
 
     columns = [
-        { "title": "ID",   "data": "id" }, 
+        { "title": "ID", "data": "id" },
         { "title": "Title", "data": "title" },
         { "title": "Body of post", "data": "body" },
-      
+
         /*,
         {  "render": function(data, type, row, meta){
             return '<a title="Delete this table" <i class="fa fa-pencil-alt"></i> </a>';
         } },*/
     ];
 
-    let dataTable = $('#dataTable').DataTable( {
-        "order": [[ 0, "asc" ]],
+    let dataTable = $('#dataTable').DataTable({
+        "order": [[0, "asc"]],
         "columns": columns
-    } );
+    });
 
-    $('#dataTable tbody').on( 'click', 'tr', function () {
-        if ( $(this).hasClass('selected') ) {
+    $('#dataTable tbody').on('click', 'tr', function () {
+        if ($(this).hasClass('selected')) {
             $(this).removeClass('selected');
         }
-        else {
-            deselect();
-            $(this).addClass('selected');
-            var table = $('#dataTable').DataTable();
-            var data = table.row(this).data();
+        deselect();
+        $(this).addClass('selected');
+        var table = $('#dataTable').DataTable();
+        var data = table.row(this).data();
 
-            // this function fetches one record and fill the modal with the data and shows the modal for editing
-            getSingleRecord(data.id, api);
+        // this function fetches one record and fill the modal with the data and shows the modal for editing
+        getSingleRecord(data.id, api);
 
-            $('#modal').modal('toggle');
-        }
+        $('#modal').modal('toggle');
     });
-   
+
 }
 
-function clear(){
+function clear() {
     $("#dataTable").DataTable().clear();
     $("#dataTable").DataTable().columns.adjust().draw();
 }
@@ -68,117 +66,142 @@ function clear(){
 function getData(api) {
 
     // asynchronous REST GET
-    $.get(api, function(data){
+    $.get(api, function (data) {
 
-        if (data){
-             $("#dataTable").DataTable().clear();
-             $("#dataTable").DataTable().rows.add(data);
-             $("#dataTable").DataTable().columns.adjust().draw();
+        if (data) {
+            $("#dataTable").DataTable().clear();
+            $("#dataTable").DataTable().rows.add(data);
+            $("#dataTable").DataTable().columns.adjust().draw();
         }
     });
- }
+}
 
- function submitNew(api){
-    
-    var formData = $("#modalForm").serializeArray().reduce(function(result, object){ result[object.name] = object.value; return result}, {});
+function getSingleRecord(id, api) {
+    console.log("Single record pre called: " + id);
+
+    apiPath = String(api + "/" + id);
+    $.get(apiPath, function (data) {
+
+        if (data) {
+            console.log("Single record post called: " + id);
+            console.log("with data: : " + data.id);
+
+            fillUpdateDiv(data, api);
+        }
+    });
+}
+
+
+function submitNew(api) {
+
+    var formData = $("#modalForm").serializeArray().reduce(function (result, object) { result[object.name] = object.value; return result }, {});
     console.log("Formdata");
     console.log(formData);
     //var id = formData.id;
-    for(var key in formData){
-        if(formData[key] == "" || formData == null) delete formData[key];
+    for (var key in formData) {
+        if (formData[key] == "" || formData == null) delete formData[key];
     }
- 
-    console.log("as string:"+JSON.stringify(formData));
- 
- 
+
+    console.log("as string:" + JSON.stringify(formData));
+
+
     $.post({
         url: api,
         data: JSON.stringify(formData),
         dataType: "json",
         success: getData(api),
-        error: function(error){
+        error: function (error) {
             console.log(error);
         }
     });
- 
+
     deselect();
     $('#modal').modal('toggle');
- }
+}
 
- // this function perform cleaning up of the table
+// this function perform cleaning up of the table
 // 1. remove eventually selected class
 // 2. clean the form using the reset method
-function deselect(){
+function deselect() {
 
     $('#dataTable tr.selected').removeClass('selected');
-    
+
     document.getElementById("modalForm").reset();
- }
+}
 
- function getSingleRecord(id, api){
 
-    apiPath = String(api +"/"+ id);
-    $.get(apiPath, function(data){
-        if (data){
-            fillUpdateDiv(data, api);
-        }
-    });
- }
- 
- 
- function fillUpdateDiv(record, api){
- 
+
+function fillUpdateDiv(record, api) {
+
     console.log(record);
     console.log(api);
- 
-    $("#btndelete").attr('onclick', 'submitDelete(' + record.id +', "' + api +'");');
-    $("#btnsubmit").attr('onclick', 'submitEdit(' + record.id +', "' + api +'");');
- 
-    document.getElementById("modal-title").innerHTML="Edit a table";
+
+    // $("#btndelete").attr('onclick', 'submitDelete(' + record.id +', "' + api +'");');
+    $("#btnsubmit").attr('onclick', 'submitEdit(' + record.id + ', "' + api + '");');
+
+    document.getElementById("modal-title").innerHTML = "Edit a table";
 
     // this function fills the modal
     fillModal(record);
+
+}
+
+function fillModal(record) {
+
+    $("#id").val(record.id);
+    $("#title").val(record.title);
+    $("#body").val(record.body);
+
     $("#confirmbutton").css('display', 'inline-block');
-    deleteID = record.id;
-    var elem = '<button type="button" class="btn btn-danger" onclick="submitDelete('+record.id+', \'' +api+'\');">Confirm delete</button>';
+    let elem = '<button type="button" class="btn btn-danger" onclick="submitDelete(' + record.id + ', \'' + api + '\');">Confirm delete</button>';
     $('#confirmbutton').popover({
-        animation:true,
-        content:elem,
-        html:true,
+        animation: true,
+        content: elem,
+        html: true,
         container: modal
     });
- }
+}
 
- function fillModal(record){
+function submitEdit(id, api) {
 
-        $("#id").val(record.id);
-        $("#title").val(record.title);
-        $("#body").val(record.body);
- }
-
- function submitEdit(id, api){
-    
-       // shortcut for filling the formData as a JavaScript object with the fields in the form
-       var formData = $("#modalForm").serializeArray().reduce(function(result, object){ result[object.name] = object.value; return result}, {});
-       console.log("Formdata =>");
-       console.log(formData);
-       var id = formData.id;
-       for(var key in formData){
-           if(formData[key] == "" || formData == null) delete formData[key];
-       }
-
-       $.ajax({
-           url: api + "/" + id,
-           type:"put",
-           data: JSON.stringify(formData),
-           dataType: "json",
-           success: getData(api),
-           error: function(error){
-               console.log(error);
-           }
-       });
-    
-       deselect();
-       $('#modal').modal('toggle');
+    // shortcut for filling the formData as a JavaScript object with the fields in the form
+    var formData = $("#modalForm").serializeArray().reduce(function (result, object) { result[object.name] = object.value; return result }, {});
+    console.log("Formdata =>");
+    console.log(formData);
+    var id = formData.id;
+    for (var key in formData) {
+        if (formData[key] == "" || formData == null) delete formData[key];
     }
- 
+
+    console.log("Updating row with id:" + id)
+    $.ajax({
+        url: api + "/" + id,
+        type: "put",
+        data: JSON.stringify(formData),
+        dataType: "json",
+        success: getData(api),
+        error: function (error) {
+            console.log(error);
+        }
+    });
+
+    deselect();
+    $('#modal').modal('toggle');
+}
+
+function submitDelete(id, api) {
+    console.log("About to remove: " + id);
+    var formData = $("#modalForm").serializeArray().reduce(function (result, object) { result[object.name] = object.value; return result }, {});
+    // rloman dit werkt niet via parameter. Waarom niet???
+    id = formData.id;
+    console.log(`Deleting row with id: ${id}`);
+    $.ajax({
+        url: api + "/" + id,
+        type: "delete",
+        dataType: 'json',
+        success: getData(api),
+        contentType: "application/json; charset=utf-8"
+    });
+
+    $('#modal').modal('toggle');
+}
