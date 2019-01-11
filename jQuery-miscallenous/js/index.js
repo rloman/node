303718e43144c8910e27f99c6268feb3a2a2fb42
Raymond Loman <raymond@carpago.nl>
2019-01-11 18:56:42 +0100

    Nice p

---
+++ jQuery-miscallenous/js/index.js
@@ -0,0 +1,46 @@
+function assert(assertion, message) {
+  if(!assertion) {
+    // console.log(message);
+    throw new Error(message);
+  }
+}
+
+function trim() {
+  let a = $.trim("              some string       ");
+  console.log(">" + a + "<");
+}
+
+function selectOnlyInputTypeText() {
+  let inputTypes = $("input[type='text']"); //...
+  console.log(inputTypes);
+  inputTypes.each(function(element) {
+    console.log(">2"+$(this).val());
+  });
+}
+
+function workingWithTheWrappedElementSet() {
+  let inputTypes = $("input[type='text']"); //...
+  assert(inputTypes.length === 1);
+  assert(inputTypes.get(0) != null);
+
+  assert(inputTypes.first() != null);
+  assert(inputTypes.last() != null);
+  // slice
+}
+
+function workingWithForms() {
+  // all checked boxed
+  $("[name='myForm']:checked").each(function() {
+    console.log($(this).val());
+  });
+
+}
+
+$(document).ready(function() {
+  trim();
+  selectOnlyInputTypeText();
+  workingWithTheWrappedElementSet();
+  workingWithForms();
+
+
+});
