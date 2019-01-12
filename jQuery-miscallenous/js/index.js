15fc3ea57f9f34f1a2bb0c01b202b2517fc13f98
Raymond Loman <raymond@carpago.nl>
2019-01-12 15:15:30 +0100

    Nice

--- jQuery-miscallenous/js/index.js
+++ jQuery-miscallenous/js/index.js
@@ -35,6 +35,35 @@ function workingWithForms() {
   });
 
 // rloman add radioset???
+}
+
+function events() {
+
+  // remove a handler
+  $(".normal").click(function() {
+    console.log("You clicked me");
+    $(".normal").unbind("click"); // removes the click handler
+    $(".once").one("click", function() {
+      console.log("You clicked me once and now I will not listen anymore");
+    })
+  });
+}
+
+// useful when you have an event handler and not a button yet
+// api has changed from 1.8 to 1.9
+// from $("selector").life("click", function() { ... }) to   =>
+function liveEventHandling() {
+
+    // bind handler to elements that might not be an the page yet
+    // Handler is added to newly created elements that match the selector
+    $("body").on("click", ".binding",  function() {
+      console.log("Live is life, Focus");
+    });
+
+    $("#addButtonButton").click(function() {
+      $("<button>Click me</button>").addClass("binding").appendTo("body");
+    });
+
 
 
 }
@@ -44,6 +73,7 @@ $(document).ready(function() {
   selectOnlyInputTypeText();
   workingWithTheWrappedElementSet();
   workingWithForms();
-
+  events();
+  liveEventHandling()
 
 });
