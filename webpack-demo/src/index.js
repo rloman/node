0cce2ba0f5bab000e998dd3f85d399d3b2eba53c
Raymond Loman <raymond@carpago.nl>
2019-01-23 11:15:31 +0100

    Add html-webpack-plugin and consequences. Love it

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,9 +1,11 @@
 import _ from 'lodash';
 import './style.css';
 import Icon from './icon.png';
+import printMe from './print.js';
 
 function component() {
     let element = document.createElement('div');
+    let btn = document.createElement('button');
 
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
@@ -11,8 +13,12 @@ function component() {
     // Add the image to our existing div.
     var myIcon = new Image();
     myIcon.src = Icon;
-
     element.appendChild(myIcon);
+
+    // Add the button to our existing div.
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+    element.appendChild(btn);
 
     return element;
 }
--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,9 +1,18 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        })
+    ],
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js', /* here name is derived from the entry names above (e.g. app and print) */
         path: path.resolve(__dirname, 'dist')
     },
     module: {
