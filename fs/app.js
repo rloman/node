9cc062d530abd29c22c63d2a214e1a4bf0b5e559
Raymond Loman <raymond@carpago.nl>
2019-03-01 09:55:40 +0100

    Add js file reading in fs

--- fs/app.js
+++ fs/app.js
@@ -16,6 +16,15 @@ app.use(function (req, res, next) {
 });
 
 
+// to open a js file which is used in index.html
+app.get('/js/index.js', function(req,res) {
+    res.writeHead(200, { 'Content-Type': 'application/js' });
+    fs.readFile('js/index.js', function(err, data) {
+      res.write(data);
+      res.end();
+    });
+})
+
 app.get('/', function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     fs.readFile('public/index.html', function (err, data) {
